f3e1e3f31773dcb3bfb6cee0120da761c2aa13d0
just-paja <pavel@zak.global>
2021-01-12 23:30:07 +0100

    Minify images during build time

---
+++ scripts/convert-images.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+root_dir=$( realpath "${script_dir}/.." )
+src_dir="${root_dir}/pixmaps"
+dest_dir="${root_dir}/src/pixmaps"
+
+mkdir -p $dest_dir
+
+for file in $src_dir/*; do
+  dest_jpg="${dest_dir}/$(basename $file)"
+  dest_webp="${dest_dir}/$(basename $file .jpg).webp"
+  convert $file -quality 90 -resize 1400x $dest_jpg
+  convert $file -quality 90 -resize 1400x $dest_webp
+done

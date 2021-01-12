41ce7235a3181b54be89b83a262f19082696c63d
just-paja <pavel@zak.global>
2021-01-13 00:09:31 +0100

    Cycle through images

--- scripts/convert-images.sh
+++ scripts/convert-images.sh
@@ -4,6 +4,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 root_dir=$( realpath "${script_dir}/.." )
 src_dir="${root_dir}/pixmaps"
 dest_dir="${root_dir}/src/pixmaps"
+manifest="${dest_dir}/manifest.json"
 
 mkdir -p $dest_dir
 
@@ -13,3 +14,6 @@ for file in $src_dir/*; do
   convert $file -quality 90 -resize 1400x $dest_jpg
   convert $file -quality 90 -resize 1400x $dest_webp
 done
+
+cd $dest_dir
+ls | grep -v manifest | jq -R -s -c 'split("\n")[:-1]' > $manifest
---
+++ src/background.js
@@ -0,0 +1,65 @@
+const manifest = './pixmaps/manifest.json'
+
+let timeout = 6000
+let index = 0
+let images = []
+let el
+let interval
+
+function loadManifest() {
+  return fetch(manifest).then(function(res) {
+    if (res.ok) {
+      return res.json()
+    }
+    return Promise.reject(res.status)
+  })
+}
+
+function initializeBackgroundsElement(file) {
+  const el = document.createElement('div')
+  el.className = 'bg'
+  document.body.appendChild(el)
+  return el
+}
+
+function setNextBackground() {
+  index = Math.floor(Math.random() * Math.floor(images.length))
+  el.style.backgroundImage = 'url(' + images[index].webp + ')'
+  console.log(el.style)
+}
+
+function runBackgrounds(backgrounds) {
+  setNextBackground()
+  interval = setInterval(function() {
+    setNextBackground()
+  }, timeout)
+}
+
+function reduceByName(aggr, item) {
+  const nameSplit = item.split('.')
+  const baseName = nameSplit.slice(0, -1).join('.')
+  const suffix = nameSplit.slice(-1)
+  const index = aggr.findIndex(function(x) { return x.name === baseName })
+  const newObj = { name: baseName }
+  newObj[suffix] = './pixmaps/' + item
+
+  if (index === -1) {
+    return aggr.concat([newObj])
+  }
+  aggr[index] = Object.assign({}, aggr[index], newObj)
+  return aggr
+}
+
+function createBackgrounds() {
+  loadManifest().then(function (files) {
+    images = files.reduce(reduceByName, [])
+    runBackgrounds()
+  })
+}
+
+function main() {
+  el = initializeBackgroundsElement()
+  createBackgrounds()
+}
+
+document.addEventListener('DOMContentLoaded', main)
